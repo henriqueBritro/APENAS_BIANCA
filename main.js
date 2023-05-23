de5dd1a5a4551f6626a0be6945acb9ad61675e94
henriqueBritro <110122296+henriqueBritro@users.noreply.github.com>
2023-05-23 15:00:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function draw(){
   image(video, 0, 250, 300, 300)
   classifier.classify(video, gotResult)
 }
-var previous_result='';
+var previousResult='';
 
 
 function gotResult(error,results) {
